0ab4175981f4f7eec9cd9c07b9b4ffefa1b78c53
Akash <acwankhede@gmail.com>
2016-03-05 13:26:01 +0530

    added urls file adding urls of login and registration modules

--- collegeTimetableSystem/collegeTimetableSystem/urls.py
+++ collegeTimetableSystem/collegeTimetableSystem/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from .views import(
+    teacher_registration,
+    teacher_login,
+    )
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^teacher_registration/$', teacher_registration),
+    url(r'^teacher_login/$', teacher_login),
 ]
